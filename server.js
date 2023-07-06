455c7c1e20793f5f099d7e3b63b7c465b5878d35
Thiago Cordeiro <thiagocordeirodantasp@Gmail.com>
2023-07-06 14:59:47 -0300

    criando servidor local

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http')
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso node',
+    '/livros': 'Entrei na pagina de Livros',
+    '/autores': 'Exibir autores'
+}
+
 const server = http.createServer((req,res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain'})
-    res.end('Curso de Node')
+    res.end(rotas[req.url])
 })
 
 
