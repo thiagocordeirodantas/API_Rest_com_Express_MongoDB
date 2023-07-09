5e4dd79505520709623211cc3cefe5cd0c7710c6
Thiago <thiagocordeirodantasd@gmail.com>
2023-07-09 17:57:51 -0300

    Evoluindo as rotas

--- src/app.js
+++ src/app.js
@@ -20,13 +20,29 @@ app.get('/', (req,res) => {
 })
 
 app.get('/livros', (req,res) => {
-    res.status(200).json(livros)
+    res.status(200).json(livros);
 })
 
 app.post('/livros', (req,res) => {
-    livros.push(req.body)
-    res.status(201).send('Livro foi cadastrado com sucesso')
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso');
 })
+
+app.put('/livros/:id', (req,res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req,res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
 
 
 export default app;
